d637e1048b27e7ef2d9e69eae45391347f1ebdb6
Annie Eby <eby.annie@gmail.com>
2018-09-11 21:16:05 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -2,7 +2,7 @@ require relative
 './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
-birth_year = gets.to_i
+birth_year = gets.to_i // gets passed in as an argument in current_age_for_birth_year
 
 users_age = current_age_for_birth_year
 
